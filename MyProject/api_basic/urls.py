2eec2278b223042f70d1420a6a86c7f91321b39e
AbuSohan <sohan@inflexionpointbd.com>
2021-12-20 09:11:52 +0600

    initial commit

---
+++ MyProject/api_basic/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    # path('detail/<int:pk>/', article_detail),
+    path('detail/<int:pk>/', ArticleDetails.as_view()),
+]
